3e0a6a3c785f6b36f0dd403289f50999965be9ef
Drew Ripberger <drew.ripberger@gmail.com>
2024-12-09 18:18:10 -0500

    run llm-load-test and build config file

--- config_convert.py
+++ config_convert.py
@@ -18,6 +18,22 @@ def flatten(d: Union[Dict[str, Any], List[Any]], prefix="") -> List[Tuple[str, A
                 elems.append(((prefix+"_"+k), v))
     return elems
 
+def template(d: Union[Dict[str, Any], List[Any]], prefix="") -> Dict:
+    resd = {}
+    if type(d) is list:
+        print("We cannot flatten a list")
+        sys.exit(1)
+    if type(d) is dict:
+        for k, v in d.items():
+            if type(v) is dict:
+                resd[k] = template(dict(v), prefix=(prefix+"_"+k))
+            elif type(v) is list:
+                print("We cannot flatten a list")
+                sys.exit(1)
+            else:
+                resd[k] = "{{ " + prefix + "_" + k + " }}"
+    return resd
+
 def main():
     if len(sys.argv) != 2:
         print(f"Incorrect number of arguments: {len(sys.argv)}")
@@ -40,6 +56,10 @@ def main():
     print("dict(")
     print("\t" + "\n\t".join([f"{param[0]} = {repr(param[1])}," for param in flat]))
     print(")")
+    print("========================================================")
+    print("============ llm-load-test Config Template =============")
+    print("========================================================")
+    print(yaml.dump(template(y, prefix="config")))
 
 if __name__ == "__main__":
     main()
